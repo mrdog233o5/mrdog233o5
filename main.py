ac5a8afc3063da59d85ea868600340eef7714916
mrdog233o5 <mr.dog233o5@gmail.com>
2023-09-30 09:20:56 +0800

    cao

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
@@ -14,6 +15,11 @@ def handle_post_request():
         message = request.get_data()
     print("message > " + message)
     open("./test.txt", "w").write(str(message))
+    token = "MTE0MTM0MDA2Mzk2ODU0MjczMA.GX75jG.frFFekrXjqk1BrAuTftSYClt1ERQn8BY2FiZgg"
+    channel_id = '1140989222556274721'
+    url = f'https://discord.com/api/v9/channels/{channel_id}/messages'
+    header = {'Authorization': f'Bot {token}'}
+    requests.post(url, headers=header, data="hello")
     return message
 
 if __name__ == '__main__':
