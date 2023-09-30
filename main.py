0d8d410f6d9548fd5f428c39e24270e4d142fa26
mrdog233o5 <mr.dog233o5@gmail.com>
2023-09-30 15:49:05 +0800

    cao

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from flask import Flask, request
 import requests
-# POST TO SERVER
-payload = {
-    'content':str('test')
-}
 
-header = {
-    "Authorization":"OTEyMDExNjQzOTQ3NDc5MDgx.GqANJ4.d1hBbj93fJQBglcbk2rs4e1x00ssBhZ5Tq2_GA"
-}
+app = Flask(__name__)
+
+@app.route('/api', methods=['POST','GET'])
+def handle_post_request():
+    try:
+        message = request.get_data().decode('utf-8')
+    except:
+        message = request.get_data()
+    print("msg > "+str(message))
+
+    # POST TO SERVER
+    payload = {
+        'content':str(message)
+    }
     
-requests.post("https://discord.com/api/v9/channels/1157577267388616725/messages", data=payload, headers=header)
+    header = {
+        "Authorization":"OTEyMDExNjQzOTQ3NDc5MDgx.GqANJ4.d1hBbj93fJQBglcbk2rs4e1x00ssBhZ5Tq2_GA"
+    }
+    
+    requests.post("https://discord.com/api/v9/channels/1157577267388616725/messages", data=payload, headers=header)
+    return message
+
+app.run()
