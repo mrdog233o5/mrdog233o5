677a3e09464836020b385fe27c960289e7076946
littleblack111 <littleblack11111@gmail.com>
2024-01-04 23:17:11 +0800

    update: added momentum, safari support(default initial position after to the animate

--- dest/js/blobCursor.js
+++ dest/js/blobCursor.js
@@ -6,6 +6,15 @@ var blob = document.getElementById("blob");
 window.onpointermove = function (event) {
   var clientX = event.clientX,
     clientY = event.clientY;
+  var momentumX = clientX + clientX - blob.offsetLeft;
+  var momentumY = clientY + clientY - blob.offsetTop;
+  blob.animate({
+    left: "".concat(momentumX, "px"),
+    top: "".concat(momentumY, "px")
+  }, {
+    duration: 1500,
+    fill: "forwards"
+  });
   blob.animate({
     left: "".concat(clientX, "px"),
     top: "".concat(clientY, "px")
@@ -13,4 +22,6 @@ window.onpointermove = function (event) {
     duration: 3000,
     fill: "forwards"
   });
+  blob.style.left = "".concat(clientX, "px");
+  blob.style.top = "".concat(clientY, "px");
 };
