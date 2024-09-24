7f3c401632de0d4ade7788118209c06db4f8ee5b
mrdog233o5 <mr.dog233o5@gmail.com>
2024-09-24 15:21:10 +0800

    reset

--- public/js/header.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var header = document.querySelector('header');
-var newElement = document.createElement('div');
-var page = header.getAttribute('page');
-console.log(page);
-newElement.innerHTML = "\n<div class=\"space\"></div>\n<div class=\"navBarContainer\">\n    <div class=\"navBar\">\n        <div class=\"url\"><a href=\"/\" id=\"btnHome\"><div class=\"center\"><h3>about</h3></div></a></div>\n        <div class=\"url\"><a href=\"/pages/programs/\" id=\"btnPrograms\"><div class=\"center\"><h3>repos</h3></div></a></div>\n    </div>\n</div>\n";
-header.appendChild(newElement);
-document.getElementById("btn" + page).setAttribute('class', 'active');
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-run() {
-    npm run watch:css &
-    npm run watch:js &
-}
-
-run
